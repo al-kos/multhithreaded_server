0ff26c969ff66fa9b96b94304b73a80ac7b6daf3
Alex Kos <kosenkoaleksey34@gmail.com>
2024-04-07 11:45:03 +0300

    add start socket dialog

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const path = require('path')
+const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
 
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+app.use(express.static(path.join(__dirname, 'public')))
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
