2e9a1b470742a21b1c03ad8e2edb13a7941cd7ee
Alex Kos <kosenkoaleksey34@gmail.com>
2024-04-08 09:02:39 +0300

    add utils

--- server.js
+++ server.js
@@ -1,29 +1,78 @@
 const path = require('path')
 const http = require('http')
+require('dotenv').config()
 const express = require('express')
 const socketio = require('socket.io')
+const formatMessage = require('./utils/messages')
+
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
-const PORT = process.env.PORT || 3000
 
+// Статическая папка
 app.use(express.static(path.join(__dirname, 'public')))
 
+// Бот-информатор
+const botName = 'ChatCord Bot'
+
+// Подключение к комнате
 io.on('connection', (socket) => {
-  console.log('New Client connected')
+  socket.on('joinRoom', ({ username, room }) => {
+    const user = userJoin(socket.id, username, room)
 
-  socket.emit('message', 'The message to client')
+    socket.join(user.room)
 
-  socket.broadcast.emit('message', 'For all clients whithout currently')
+    // Приветствие, информация о подключении
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'))
 
-  socket.on('disconnect', () => {
-    io.emit('message', 'Client disconnect')
+    // Уведомление о присоединении для всех пользователей
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        'message',
+        formatMessage(botName, `${user.username} has joined the chat`),
+      )
+
+    // Отображение всех пользователей
+    io.to(user.room).emit('roomUsers', {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    })
   })
 
+  // Слушатель сообщений из чата
   socket.on('chatMessage', (msg) => {
-    console.log(msg)
+    const user = getCurrentUser(socket.id)
+
+    io.to(user.room).emit('message', formatMessage(user.username, msg))
+  })
+
+  // Уведомление об отсоединении для всех пользователей
+  socket.on('disconnect', () => {
+    const user = userLeave(socket.id)
+
+    if (user) {
+      io.to(user.room).emit(
+        'message',
+        formatMessage(botName, `${user.username} has left the chat`),
+      )
+
+      // Отображение всех пользователей
+      io.to(user.room).emit('roomUsers', {
+        room: user.room,
+        users: getRoomUsers(user.room),
+      })
+    }
   })
 })
+
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
